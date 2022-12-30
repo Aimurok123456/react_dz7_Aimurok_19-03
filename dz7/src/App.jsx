0c8dad059267b927f3cdcdca13d6b6d4fd15da6f
Aimurok123456 <115090521+Aimurok123456@users.noreply.github.com>
2022-12-30 15:24:30 +0600

    Add files via upload

---
+++ dz7/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import PostList from './components/postList/postList'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <PostList/>
+    </div>
+  )
+}
+
+export default App
---
+++ userId/src/App.jsx
@@ -0,0 +1,14 @@
+import PostList from './components/postList/postList'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <PostList/>
+    </div>
+  )
+}
+
+export default App
